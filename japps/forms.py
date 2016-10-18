4bd8d417af23254e2962514f4a642d3b461da8ee
aliceminotto <i.m.e@hotmail.it>
2016-10-18 12:07:28 +0100

    added input fields and additional infomations as maximum cardinality and choice field.

--- japps/forms.py
+++ japps/forms.py
@@ -7,62 +7,7 @@ from . import models
 class NameJob(forms.Form):
     name_job=forms.CharField(label="Job Name", max_length=50, required=True)
 
-class ApplicationForm(ModelForm):
-    value=forms.CharField(label="")
-
-    class Meta:
-        model=models.Input
-        fields="__all__"
-
-class NumericalForm(ModelForm):
-    value=forms.FloatField(label="")
-
-    class Meta:
-        model=models.NumericalParameter
-        fields="__all__"
-
-class BooleanForm(ModelForm):
-    value=forms.BooleanField(label="")
-
-    class Meta:
-        model=models.BooleanParameter
-        fields="__all__"
-
-class TextForm(ModelForm):
-    value=forms.CharField(label="")
-
-    class Meta:
-        model=models.TextParameter
-        fields="__all__"
-
 class ParameterForm(forms.Form):
 
     def __init__(self, *args, **kwargs) :
-        pass
-        """valuename=args[0]
-        parameter_type = kwargs.pop("parameter_type")
-        parameter_label = kwargs.pop("parameter_label")
-        parameter_description = kwargs.pop("parameter_description")
-        super(ParameterForm,self).__init__()
-        if parameter_type=="string":
-            self.fields[valuename]=forms.CharField(max_length=50)
-            self.fields[valuename].label=parameter_label
-            self.fields[valuename].help_text=parameter_description ###note this may be empty
-            #self.fields["value"].default=parameter_default ###note this may be empty as well
-            #if parameter_regex!="": ###synthax? -set it as default if not given--
-            #    self.fields["value"].validators=parameter_regex
-            #else:
-            #    pass #leaving this here as it may give errors?
-        elif parameter_type=="number":
-            valuename=forms.FloatField(label=parameter_label)
-            self.valuename.help_text=parameter_description
-            #if parameter_regex!="":
-            #    value=forms.FloatField(label=parameter_label, validators=parameter_regex)
-            #else:
-            #    value=forms.FloatField(label=parameter_label, validators=parameter_regex)
-        elif parameter_type=="bool" or parameter_type=="flag":
-            valuename=forms.BooleanField(label=parameter_label)
-            self.valuename.help_text=parameter_description
-        else:
-            #should never be here as agave validate the json
-            return "error: parameter type not valid." """
+        super(ParameterForm,self).__init__(*args,**kwargs)
--- japps/views.py
+++ japps/views.py
@@ -1,5 +1,6 @@
 import os
 import json
+from collections import OrderedDict
 
 from django import forms
 from django.views import generic
@@ -8,36 +9,52 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.conf import settings
 
-from .forms import ApplicationForm, NameJob, NumericalForm, BooleanForm, TextForm, ParameterForm
+from .forms import ParameterForm
 
-a=open(os.path.join(settings.PROJECT_ROOT, 'GWasserApp.json'))
+a=open(os.path.join(settings.PROJECT_ROOT, 'MikadoApp.json'))
 ex_json=json.load(a)
 
+def additional_features(field):
+    global fields
+    fields[field["id"]].label=field["details"].get("label", field["id"])
+    fields[field["id"]].help_text=field["details"].get("description", "")
+    if field["value"].get("default")!=None:
+        fields[field["id"]].default=field["value"].get("default")
+    if field["value"].get("validator")!=None:
+        fields[field["id"]].validators=field["value"].get("validator")
+    if field["value"].get("required")!=True:
+        fields[field["id"]].required=False
+
 def get_name(request):
+    global fields
     if request.method=='POST':
-        form=ApplicationForm(request.POST)
-        name_form=NameJob(request.POST, initial={"name_job":ex_json["name"]})
-        num_form=NumericalForm(request.POST)
-        bool_form=BooleanForm(request.POST)
-        text_form=TextForm(request.POST)
         nice_form=ParameterForm(request.POST)
-        if form.is_valid() and name_form.is_valid() and num_form.is_valid() and bool_form.is_valid() and text_form.is_valid() and nice_form.is_valid():
+        if nice_form.is_valid():
             return HttpResponseRedirect('/job_submitted/')
     else:
-        form=ApplicationForm()
-        name_form=NameJob(initial={"name_job":ex_json["name"]})
-        num_form=NumericalForm()
-        bool_form=BooleanForm()
-        text_form=TextForm()
-        fields = {};
+        fields=OrderedDict()
+        fields["name_job"]=forms.CharField(initial=ex_json["name"])
+        for field in ex_json["inputs"]:
+            if field.get("semantics")!=None:
+                if field["semantics"].get("maxCardinality")>1 or field["semantics"].get("maxCardinality")==-1:
+                    fields[field["id"]]=forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+                else:
+                    fields[field["id"]]=forms.FileField()
+            else:
+                fields[field["id"]]=forms.FileField()
+            additional_features(field)
         for field in ex_json["parameters"]:
-            if field["value"]["type"] == "string":
-                fields[field["id"]] = forms.CharField()
-            elif field["value"]["type"] == "number":
-                fields[field["id"]] = forms.FloatField()
-            elif field["value"]["type"] == "bool" or field["value"]["type"] == "flag":
-                fields[field["id"]] = forms.BooleanField()
-            fields[field["id"]].label=field["details"].get("label", field["id"])
-            fields[field["id"]].help_text=field["details"].get("description", "")
+            if field["value"].get("type")==None:
+                return "error" #should nver be here as this check is done by agave
+            else:
+                if field["value"]["type"]=="string":
+                    fields[field["id"]]=forms.CharField(max_length=50)
+                elif field["value"]["type"]=="number":
+                    fields[field["id"]]=forms.FloatField()
+                elif field["value"]["type"]=="bool" or field["value"]["type"]=="flag":
+                    fields[field["id"]]=forms.BooleanField()
+                elif field["value"]["type"]=="enumeration":
+                    fields[field["id"]]=forms.ChoiceField(choices=enumerate(field["value"]["enumValues"]))
+            additional_features(field)
         nice_form=type('ParameterForm', (forms.BaseForm,), {'base_fields': fields})
-    return render(request, 'japps/submission.html', {'form': form, 'json_app': ex_json, 'name_form': name_form, 'num_form': num_form, "bool_form": bool_form, "text_form": text_form, "nice_form": nice_form } )
+    return render(request, 'japps/submission.html', { 'json_app': ex_json, "nice_form": nice_form } )
