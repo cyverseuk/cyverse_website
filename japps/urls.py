12620477b4d71e24d89bff0b78ef45cef93572d9
aliceminotto <i.m.e@hotmail.it>
2016-11-17 17:12:54 +0000

    redirect submission/ to a working index page changing the url itself

--- japps/urls.py
+++ japps/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 from . import views
 
@@ -8,7 +10,7 @@ urlpatterns = [
     #url(r'^$', views.IndexView.as_view(), name="index"),
     url(r'^$',views.list_apps,name='index'),
     # ex: /japps/submission.html/
-    url(r'^submission/$', views.list_apps),
+    url(r'^submission/$', RedirectView.as_view(url=reverse_lazy('japps:index')), name='go-to-index'),
     url(r'^submission/(?P<application>[\.\w-]+)$', views.create_form, name='submission'),
     url(r'^job_submitted/$', views.submitted, name='job_submitted' ),
 ]
