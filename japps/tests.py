86443a0f62fec69f98709e9056ef681c542ccb73
aliceminotto <i.m.e@hotmail.it>
2016-11-17 17:20:03 +0000

    more test, last one is failing, to be fixed

--- japps/tests.py
+++ japps/tests.py
@@ -24,7 +24,7 @@ class IndexTest(TestCase):
 
     def test_loginform(self):
         """
-        test if the form is valid
+        test if the form is valid when receiving a non blank string
         """
         token_form=get_token()(data={"user_token": missing_token})
         #print token_form.errors
@@ -36,3 +36,29 @@ class IndexTest(TestCase):
         token_form=get_token()(data={"user_token": valid_token})
         self.assertTrue(token_form.is_valid())
         self.assertTrue(token_form.is_bound)
+
+class AppTest(TestCase):
+
+    def test_app(self):
+        """
+        test that you can reach the page
+        """
+        ex_app="/GWasser-1.0.0u1"
+        risposta=self.client.get('/submission'+ex_app)
+        self.assertEqual(risposta.status_code, 200)
+
+    def test_redirection(self):
+        """
+        test that you can reach the page
+        """
+        ex_app=""
+        risposta=self.client.get('/submission'+ex_app)
+        self.assertEqual(risposta.status_code, 301) #this is the code for redirection
+
+    def test_unexisting_app(self):
+        """
+        test that invalid app name return 404
+        """
+        ex_app="/im_not_a_valid_app"
+        risposta=self.client.get('/submission'+ex_app)
+        self.assertEqual(risposta.status_code, 404)
