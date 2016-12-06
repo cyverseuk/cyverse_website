bf6d0b0b0723b122161fab39ed864577a3dc3032
aliceminotto <i.m.e@hotmail.it>
2016-12-06 16:53:48 +0000

    this work for a single radio button

--- japps/static/japps/upload_button.js
+++ japps/static/japps/upload_button.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-  if($('input[type="radio"]:checked').val()=="url") {
-            ($(this).closest('td').next('td').next('td')).prop("disabled", false);
+    $('input[type="radio"]').click(function() {
+        if($('input[type="radio"]:checked').val()=="url") {
+            ($(this).closest('tr').next('tr').children("td").children("input")).prop("disabled", true);
+            ($(this).closest('tr').next('tr').next('tr').children("td").children("input")).prop("disabled", false);
+        }
+        else {
+            ($(this).closest('tr').next('tr').children("td").children("input")).prop("disabled", false);
+            ($(this).closest('tr').next('tr').next('tr').children("td").children("input")).prop("disabled", true);
         };
 });
 });
