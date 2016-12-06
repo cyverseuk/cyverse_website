d1e9af37905753403548d0e27b8460c9ea648d15
aliceminotto <i.m.e@hotmail.it>
2016-12-06 17:09:51 +0000

    this work for each radio button

--- japps/static/japps/upload_button.js
+++ japps/static/japps/upload_button.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('input[type="radio"]').click(function() {
-        if($('input[type="radio"]:checked').val()=="url") {
+        if($(this).filter(':checked').val()=="url") {
             ($(this).closest('tr').next('tr').children("td").children("input")).prop("disabled", true);
             ($(this).closest('tr').next('tr').next('tr').children("td").children("input")).prop("disabled", false);
         }
