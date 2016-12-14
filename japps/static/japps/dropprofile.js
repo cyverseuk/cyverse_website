3af6de7e04fe44fc714e94a4ceeb39c2c4beafc0
aliceminotto <i.m.e@hotmail.it>
2016-12-14 11:36:13 +0000

    very basic script for toggling dropdown menu

--- japps/static/japps/dropprofile.js
+++ japps/static/japps/dropprofile.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $(".dropbtn").click(function() {
     $("#prof_dropdown").toggle();
   });
-  $(document).click(function(event) {
-    if(!$(event.target).closest('#prof_dropdown').length) {
-        if($('#prof_dropdown').is(":visible")) {
-            $('#prof_dropdown').hide();
-        };
-    };        
-  });
+  //$(document).click(function(event) {
+    //if(!$(event.target).closest('#prof_dropdown').length) {
+      //  if($('#prof_dropdown').is(":visible")) {
+        //    $('#prof_dropdown').hide();
+        //};
+    //};
+  //});
 });
